71566cd840eb395e9cfc1a71f6f14b7c1dfa78f1
YassineElGhizi <86019502+YassineElGhizi@users.noreply.github.com>
2021-08-02 18:40:38 +0100

    8-2-2021-6-40

--- frontend/src/components/result.js
+++ frontend/src/components/result.js
@@ -5,77 +5,98 @@ import DataTableExtensions from "react-data-table-component-extensions";
 
 import './result.css';
 
-const data = [
-    { id: 1, title: 'yassine', year: 12 } ,
-    { id: 2, title: 'salim', year: 25 }, 
-    { id: 3, title: 'adel', year: 4 }, 
-    { id: 4, title: 'oussama', year: 80 }, 
-    { id: 5, title: 'adnan', year: 52 }, 
-    { id: 6, title: 'hafssa', year: 78 }, 
-    { id: 8, title: 'godzilla', year: 45 }, 
-    { id: 9, title: 'noussaib', year: 54 }, 
-    { id: 14, title: 'engenner man', year: 88 }, 
-    { id: 15, title: 'brian', year: 88.80 }, 
-    { id: 16, title: 'ain saiss', year: 78 }, 
-    { id: 17, title: 'oud nill', year: 1000 }, 
-    ];
+// const data = [
+//     { id: 1, title: 'yassine', year: 12 } ,
+//     { id: 2, title: 'salim', year: 25 }, 
+//     { id: 3, title: 'adel', year: 4 }, 
+//     { id: 4, title: 'oussama', year: 80 }, 
+//     { id: 5, title: 'adnan', year: 52 }, 
+//     { id: 6, title: 'hafssa', year: 78 }, 
+//     { id: 8, title: 'godzilla', year: 45 }, 
+//     { id: 9, title: 'noussaib', year: 54 }, 
+//     { id: 14, title: 'engenner man', year: 88 }, 
+//     { id: 15, title: 'brian', year: 88.80 }, 
+//     { id: 16, title: 'ain saiss', year: 78 }, 
+//     { id: 17, title: 'oud nill', year: 1000 }, 
+//     ];
 
-const columns = [
-  {
-    name: 'Title',
-    selector: 'title',
-    sortable: true,
-  },
-  {
-    name: 'Year',
-    selector: 'year',
-    sortable: true,
-    right: true,
-  },
-];
-const tableData = {columns,data}
+// const columns = [
+//   {
+//     name: 'Title',
+//     selector: 'title',
+//     sortable: true,
+//   },
+//   {
+//     name: 'Year',
+//     selector: 'year',
+//     sortable: true,
+//     right: true,
+//   },
+// ];
+// const tableData = {columns,data}
 
 export default class Result extends React.Component
 {
     constructor(props) {
         super(props);
         this.state = {
-            columns : [{
-                name: 'Title',
-                selector: 'title',
-                sortable: true,
-              },
-              {
-                name: 'Year',
-                selector: 'year',
-                sortable: true,
-                right: true,
-              },],
-            tableData00 : ""
+            data : [
+                { id: 1, title: 'yassine', year: 12 } ,
+                { id: 2, title: 'salim', year: 25 }, 
+                { id: 3, title: 'adel', year: 4 }, 
+                { id: 4, title: 'oussama', year: 80 }, 
+                { id: 5, title: 'adnan', year: 52 }, 
+                { id: 6, title: 'hafssa', year: 78 }, 
+                { id: 8, title: 'godzilla', year: 45 }, 
+                { id: 9, title: 'noussaib', year: 54 }, 
+                { id: 14, title: 'engenner man', year: 88 }, 
+                { id: 15, title: 'brian', year: 88.80 }, 
+                { id: 16, title: 'ain saiss', year: 78 }, 
+                { id: 17, title: 'oud nill', year: 1000 }, 
+                ],
+            columns : [
+                {
+                    name: 'Title',
+                    selector: 'title',
+                    sortable: true,
+                  },
+                  {
+                    name: 'Year',
+                    selector: 'year',
+                    sortable: true,
+                    right: true,
+                  }
+            ],
+            // tableData: {
+            //     columns: { ...this.state.columns },
+            //     data: { ...this.state.data }
+            //   }
+            // tableData: {
+            //     columns: Object.assign({}, this.state.columns),
+            //     data: Object.assign({}, this.state.data)
+            //   }
         };
     }
-
-    
+        
     render(){  
         return(
             <div>   
                 <div style={{width: '65%' , marginLeft : '20%' , marginTop : '2vw'}}>
                     <DataTableExtensions
-                        {...tableData}
+                        {...this.state.tableData}
                         export={false}
                         print={false}
                     >
                         <DataTable
                         title="Test Data Table"
                         columns={this.state.columns}
-                        data={data}
+                        data={this.state.data}
                         pagination
                         // defaultSortFieldId={1}
                         defaultSortField="id"
                         highlightOnHover
                         paginationPerPage={6}
                         paginationRowsPerPageOptions={[6 , 8]}
-
                         />
                     </DataTableExtensions>
                 </div>   
